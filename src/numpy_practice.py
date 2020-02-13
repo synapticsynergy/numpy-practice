c41b82115895f3b97f1102aaefc86befc145a7ef
Blake Danson <blakedanson@gmail.com>
2020-02-12 22:56:51 -0800

    Add shape tests

--- src/numpy_practice.py
+++ src/numpy_practice.py
@@ -3,3 +3,16 @@ import numpy as np
 
 def three_zeros():
     return np.zeros(3)
+
+
+def five_zeros():
+    return np.zeros(5)
+
+
+def array_shape(np_array):
+    return np_array.shape
+
+
+def reshape_array(np_array, shape):
+    np_array.shape = shape
+    return np_array.shape
--- tests/test_numpy_practice.py
+++ tests/test_numpy_practice.py
@@ -6,3 +6,24 @@ from src import numpy_practice
 def test_three_zeros():
     result = numpy_practice.three_zeros()
     assert (result == [0., 0., 0.]).all()
+    assert type(result) == np.ndarray
+    assert type(result[0]) == np.float64
+
+
+def test_five_zeros():
+    result = numpy_practice.five_zeros()
+    assert (result == [0., 0., 0., 0., 0.]).all()
+    assert type(result) == np.ndarray
+    assert type(result[0]) == np.float64
+
+
+def test_array_shape():
+    test_array = np.zeros(10)
+    result = numpy_practice.array_shape(test_array)
+    assert result == (10,)
+
+
+def test_reshape_array():
+    test_array = np.zeros(10)
+    result = numpy_practice.reshape_array(test_array, (5, 2))
+    assert result == (5, 2)
