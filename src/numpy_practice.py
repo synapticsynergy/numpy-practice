355c356b2318282e298e176d2f837eb08a0194cf
Blake Danson <blakedanson@gmail.com>
2020-02-14 17:47:50 -0800

    Add dims, ones, linspace

--- src/numpy_practice.py
+++ src/numpy_practice.py
@@ -9,10 +9,28 @@ def five_zeros():
     return np.zeros(5)
 
 
+def seven_ones():
+    return np.ones(7)
+
+
+def convert_to_nparray(array):
+    print(np.array(array).ndim)
+    return np.array(array)
+
+
 def array_shape(np_array):
     return np_array.shape
+
+
+def get_dimensions(np_array):
+    return np_array.ndim
 
 
 def reshape_array(np_array, shape):
     np_array.shape = shape
     return np_array
+
+
+def get_linspace(start, stop, num):
+    print(np.linspace(start, stop, num))
+    return np.linspace(start, stop, num)
--- tests/test_numpy_practice.py
+++ tests/test_numpy_practice.py
@@ -35,6 +35,37 @@ def test_five_zeros():
     assert type(result[0]) == np.float64
 
 
+@pytest.mark.describe('seven_ones()')
+@pytest.mark.it('It should be a function')
+def test_seven_ones_function():
+    result = numpy_practice.seven_ones
+    assert result
+
+
+@pytest.mark.describe('seven_ones()')
+@pytest.mark.it('It should return a numpy array containing five zeros')
+def test_seven_ones():
+    result = numpy_practice.seven_ones()
+    assert (result == [1., 1., 1., 1., 1., 1., 1.]).all()
+    assert type(result) == np.ndarray
+    assert type(result[0]) == np.float64
+
+
+@pytest.mark.describe('convert_to_nparray()')
+@pytest.mark.it('It should be a function')
+def test_convert_to_nparray_function():
+    result = numpy_practice.convert_to_nparray
+    assert result
+
+
+@pytest.mark.describe('convert_to_nparray()')
+@pytest.mark.it('It should accept a list and return a matching numpy array')
+def test_convert_to_nparray():
+    test_list = [1, 2, 3, 4, 5]
+    result = numpy_practice.convert_to_nparray(test_list)
+    assert (result == test_list).all()
+
+
 @pytest.mark.describe('array_shape()')
 @pytest.mark.it('It should be a function')
 def test_array_shape_function():
@@ -50,6 +81,21 @@ def test_array_shape():
     assert result == (10,)
 
 
+@pytest.mark.describe('get_dimensions()')
+@pytest.mark.it('It should be a function')
+def test_get_dimensions_function():
+    result = numpy_practice.get_dimensions
+    assert result
+
+
+@pytest.mark.describe('get_dimensions()')
+@pytest.mark.it('It should return a reshaped numpy array')
+def test_get_dimensions():
+    test_array = [[[[[[[[1, 2, 3]]]]]]]]
+    result = numpy_practice.get_dimensions(np.array(test_array))
+    assert result == 8
+
+
 @pytest.mark.describe('reshape_array()')
 @pytest.mark.it('It should be a function')
 def test_reshape_array_function():
@@ -63,3 +109,18 @@ def test_reshape_array():
     test_array = np.zeros(10)
     result = numpy_practice.reshape_array(test_array, (5, 2))
     assert result.shape == (5, 2)
+
+
+@pytest.mark.describe('get_linspace()')
+@pytest.mark.it('It should be a function')
+def test_get_linspace_function():
+    result = numpy_practice.get_linspace
+    assert result
+
+
+@pytest.mark.describe('get_linspace()')
+@pytest.mark.it('It should return a numpy array that specifies the possible start and end numbers, and number of elements to return')
+def test_get_linspace_nparray():
+    result = numpy_practice.get_linspace(5, 20, 9)
+    assert (result == [5., 6.875, 8.75, 10.625,
+                       12.5, 14.375, 16.25, 18.125, 20.]).all()
